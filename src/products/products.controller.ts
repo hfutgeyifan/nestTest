c1026c17cce113f2e621f161961c60ffdf59ea0b
shyfge <529594222@qq.com>
2026-09-09 22:00:14 +0800

    1

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,8 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateProductDto } from './dto/createProduct';
 
 @Controller('products')
 export class ProductsController {
+    @Get('getAllProducts')
+    findAll(): CreateProductDto[] {
+        return [];
+    }
+
     @Post("create")
     async create(@Body() createProductDto: CreateProductDto) {
         return 'This action adds a new cat';
